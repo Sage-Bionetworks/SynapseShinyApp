c38b1bb5d8335102b11cf0e833852694beb52736
kdaily <kdaily@users.noreply.github.com>
2015-12-07 14:01:33 -0800

    move inside observeEvent

--- server.R
+++ server.R
@@ -16,21 +16,23 @@ shinyServer(function(input, output, session) {
   session$sendCustomMessage(type="readCookie",
                             message=list(name='org.sagebionetworks.security.user.login.token'))
   
-  foo <- reactive(synapseLogin(sessionToken=input$cookie))
-
-  output$title <- renderUI({
-    titlePanel(sprintf("Welcome, %s", synGetUserProfile()@userName))
-  })
-  
-  output$distPlot <- renderPlot({
-
-    # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2]
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-    # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
-
-  })
-
+  foo <- observeEvent(input$cookie, {
+    
+    synapseLogin(sessionToken=input$cookie)
+    
+    output$title <- renderUI({
+      titlePanel(sprintf("Welcome, %s", synGetUserProfile()@userName))
+    })
+    
+    output$distPlot <- renderPlot({
+      
+      # generate bins based on input$bins from ui.R
+      x    <- faithful[, 2]
+      bins <- seq(min(x), max(x), length.out = input$bins + 1)
+      
+      # draw the histogram with the specified number of bins
+      hist(x, breaks = bins, col = 'darkgray', border = 'white')
+      
+    })
+  })    
 })
