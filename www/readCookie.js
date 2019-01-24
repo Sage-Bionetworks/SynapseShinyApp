79190e1e83a733ec1e1ee410fe7f5816ae1c9518
kdaily <kenneth.daily@sagebase.org>
2019-01-24 14:15:46 -0800

    add withcredentials

--- www/readCookie.js
+++ www/readCookie.js
@@ -7,6 +7,7 @@ Shiny.addCustomMessageHandler("readCookie", function(message) {
 function readCookie() {
   const Http = new XMLHttpRequest();
   const url='https://staging.synapse.org/Portal/sessioncookie';
+  Http.withCredentials = true;
   Http.open("GET", url);
   Http.send();
   Http.onreadystatechange=(e)=>{return(Http.responseText)};
