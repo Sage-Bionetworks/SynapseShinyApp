3d154ca1ee999c1dafd7de94e684e328729ec255
kdaily <kenneth.daily@sagebase.org>
2019-01-24 13:35:59 -0800

    use new endpoint to get session cookie

--- www/readCookie.js
+++ www/readCookie.js
@@ -5,13 +5,9 @@ Shiny.addCustomMessageHandler("readCookie", function(message) {
 });
 
 function readCookie() {
-	name = "org.sagebionetworks.security.user.login.token";
-	var nameEQ = name + "=";
-	var ca = document.cookie.split(';');
-	for(var i=0;i < ca.length;i++) {
-		var c = ca[i];
-		while (c.charAt(0)==' ') c = c.substring(1, c.length);
-		if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
-	}
-	return null;
+  const Http = new XMLHttpRequest();
+  const url='https://staging.synapse.org/Portal/sessioncookie';
+  Http.open("GET", url);
+  Http.send();
+  Http.onreadystatechange=(e)=>{return(Http.responseText)};
 }
