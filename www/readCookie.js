e7db2caf028c57f40179e956e12330c39e440bb4
Kara Woo <kara.woo@sagebase.org>
2019-04-03 14:28:47 -0700

    Show modal message to user if not logged in
    Closes #5

--- www/readCookie.js
+++ www/readCookie.js
@@ -8,10 +8,14 @@ function readCookie() {
   const url='https://www.synapse.org/Portal/sessioncookie';
   xhr.withCredentials = true;
   xhr.onreadystatechange = function() {
-     if (xhr.readyState == XMLHttpRequest.DONE) {
-         Shiny.onInputChange("cookie",xhr.responseText);
-     }
-  }
+    if (xhr.readyState == XMLHttpRequest.DONE) {
+      if (xhr.status == 401) {
+        Shiny.onInputChange("authorized", false);
+      } else {
+        Shiny.onInputChange("cookie",xhr.responseText);
+      }
+    };
+  };
   xhr.open("GET", url);
   xhr.send();
 }
